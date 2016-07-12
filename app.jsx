8b1f07cf9d5baef7a50da0588e0f32bead429d9e
Shawn Daichendt <shawn.daichendt@gmail.com>
2016-07-12 10:36:23 -0400

    removed bad files from root

--- app.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Button } from 'react-bootstrap';
-const buttonsInstance = (
-  <Button>Click me!</Button>
-);
-ReactDOM.render(buttonsInstance, document.getElementById('here'));
