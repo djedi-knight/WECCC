f0288cbae62f439984998c961d37f0498d7b2af4
Shawn Daichendt <shawn.daichendt@gmail.com>
2016-07-27 12:35:18 -0400

    added NavBar/ SignoutButton jsx files

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import PureRenderMixin from 'react-addons-pure-render-mixin';
+
+export default React.createClass({
+  propTypes: {
+    children: React.PropTypes.node
+  },
+  mixins: [PureRenderMixin],
+  render() {
+    //  TODO: put render code here
+    return;
+  }
+});
