ceb10a2a8218dcfdd4a144382eade84aa54948fb
Dee <adepojua@uwindsor.ca>
2016-07-27 14:13:00 -0400

    Initial .scss styling of home page Component

--- src/components/HeaderImage/HeaderImage.jsx
+++ src/components/HeaderImage/HeaderImage.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import { Card, CardMedia, CardTitle} from 'react-toolbox/lib/card';
+import style from './style';
 
-// const HeaderText = 
+let className = style.headerimage; 
 
 class HeaderImage extends React.Component{
   render(){
     return(
-      <div>
+      <div className={className}>
         <h1 style={{ paddingTop: 10, paddingRright: 10, paddingLeft: 10 }}>Improving Value by Exploring Data</h1>  
         <CardMedia aspectRatio="wide" image = "https://placeimg.com/800/450/nature"/>
         <i><p>This tool has been developed by the ENCOMPASS Community project.</p></i>
         <i><p>It is an opportunity to allow home, community, long-term home providers and mangers as well as community 
-        professionals to explore and to understand the outcomes of their efforts on the population they serve</p></i>
+        professionals to explore and to understand the outcomes of their efforts on the population they serve.</p></i>
       </div>
     );
   }
