0e3ca7f33b00e08b9871ad2e8aabbcec8b26786c
Shawn Daichendt <shawn.daichendt@gmail.com>
2016-07-28 11:00:26 -0400

    formatting updates

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
+import PureRenderMixin from 'react-addons-pure-render-mixin';
 import AppBar from 'react-toolbox/lib/app_bar';
 
-class AppBarTest extends React.Component{
-	render(){
-		return(
-			<div>
-				<AppBar fixed flat > Outcome Reports </AppBar>
-			</div>
+export default React.createClass({
+  mixins: [PureRenderMixin],
+  render() {
+    return (
+      <div>
+        <AppBar fixed flat >Outcome Reports</AppBar>
+      </div>
 		);
-	}
-}
-
-export default AppBarTest;
+  }
+});
--- src/components/Layout/LayoutTest.jsx
+++ src/components/Layout/LayoutTest.jsx
@@ -1,26 +1,21 @@
 import React from 'react';
-import { AppBar, Checkbox, IconButton } from 'react-toolbox';
-import { Layout, NavDrawer, Panel, Sidebar } from 'react-toolbox';
+import PureRenderMixin from 'react-addons-pure-render-mixin';
+import { AppBar, Layout, Panel } from 'react-toolbox';
 import HeaderImage from '../HeaderImage/HeaderImage.jsx';
 
-
-class LayoutTest extends React.Component {    
-
-    render() {
-        return (
-            <Layout>
-                
-                <Panel>
-                    <AppBar fixed flat >Outcome Reports</AppBar>
-                    <br /><br /><br /><br />
-                    <div style={{ display: 'block',  width: '50%', margin: 'auto', padding: 10 }}>
-                        <HeaderImage />
-                    </div>
-                </Panel>
-                
-            </Layout>
-        );
-    }
-}
-
-export default LayoutTest;
+export default React.createClass({
+  mixins: [PureRenderMixin],
+  render() {
+    return (
+      <Layout>
+        <Panel>
+          <AppBar fixed flat >Outcome Reports</AppBar>
+          <br /><br /><br /><br />
+          <div style={{ display: 'block', width: '50%', margin: 'auto', padding: 10 }}>
+            <HeaderImage />
+          </div>
+        </Panel>
+      </Layout>
+		);
+  }
+});
