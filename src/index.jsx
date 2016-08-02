13cef0bf5d196dabac41d2533e3dda4f1d623535
stephenwaskew <stephenwaskew@gmail.com>
2016-08-02 10:26:19 -0400

    moved stylesheet

--- src/index.jsx
+++ src/index.jsx
@@ -8,7 +8,8 @@ import remoteActionMiddleware from './actions/remote_action_middleware';
 import App from './components/App';
 import { HomeContainer } from './components/Home';
 import { MyGroupsContainer } from './components/MyGroups/MyGroups';
-require('./style.css');
+require('./theme/style.css');
+
 // setup React-Redux store
 const createStoreWithMiddleware = applyMiddleware(
   remoteActionMiddleware
