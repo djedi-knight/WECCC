2a6c3a04cce4962453394824303a582ded4ee53b
stephenwaskew <stephenwaskew@gmail.com>
2016-08-03 14:10:42 -0400

    switch branch. updated modal

--- src/components/PracticeMode/PracticeMode.jsx
+++ src/components/PracticeMode/PracticeMode.jsx
@@ -1,35 +1,39 @@
 import Dialog from 'react-toolbox/lib/dialog';
 import React from 'react';
 import PureRenderMixin from 'react-addons-pure-render-mixin';
+import Button from 'react-toolbox';
 
 export default React.createClass({
   mixins: [PureRenderMixin],
+
   getInitialState() {
-    return { active: true }
-  }
+    return { active: true };
+  },
 
-  handleToggle () {
-    this.setState({active: !this.state.active});
-  }
+  handleToggle() {
+    this.setState({active: !this.state.active})
+  },
 
-  actions = [
+  actions: [
+
     { label: "Cancel" },
-    { label: "Save" }
-  ];
+    { label: "Save" },
+  ],
 
   render () {
     return (
       <div>
+      <Button label='Show my dialog' onClick={this.handleToggle} />
         <Dialog
           actions={this.actions}
           active={this.state.active}
           onEscKeyDown={this.handleToggle}
           onOverlayClick={this.handleToggle}
-          title='My awesome dialog'
+          title='My First Dialog'
         >
           <p>Here you can add arbitrary content. Components like Pickers are using dialogs now.</p>
         </Dialog>
       </div>
     );
   }
-}
+ });
