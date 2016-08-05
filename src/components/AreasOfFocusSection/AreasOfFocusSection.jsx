d00f1423f4c8388bbf4d7775c439a55571e32b47
chang11n <chang11n@uwindsor.ca>
2016-08-05 14:08:42 -0400

    AreasOfFocusSection.jsx update

--- src/components/AreasOfFocusSection/AreasOfFocusSection.jsx
+++ src/components/AreasOfFocusSection/AreasOfFocusSection.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import PureRenderMixin from 'react-addons-pure-render-mixin';
-import { Card, CardMedia, CardTitle, CardText, CardActions} from 'react-toolbox/lib/card';
-import Button from 'react-toolbox/lib/button';
+import { Card, CardText} from 'react-toolbox/lib/card';
 
-const dummyText = 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.';
 
 export default React.createClass({
   mixins: [PureRenderMixin],
@@ -15,8 +13,7 @@ export default React.createClass({
                 THINGS TO CONSIDER
             </h3>
                 <CardText>		
-                       <div>
-                             
+                       <div>                             
                                <label><input type="checkbox" />Does my organizationhave the conections and communciations it needs to engage others?</label><br/><hr/>
                                <label><input type="checkbox" />Is the distribution and outcomes of patients condistent with ourt organizational/HR strategies and in-house expertise?</label><br/><hr/>
                                <label><input type="checkbox" />Does my organization have the staff-wide training programs it needs to ensure we add value?</label><br/><hr/>
