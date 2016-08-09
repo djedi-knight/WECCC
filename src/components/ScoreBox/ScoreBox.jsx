a2c7cf0ce747d1948a698abeda70856b4241ef4b
stephenwaskew <stephenwaskew@gmail.com>
2016-08-09 12:14:11 -0400

    added to score box component

--- src/components/ScoreBox/ScoreBox.jsx
+++ src/components/ScoreBox/ScoreBox.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import PureRenderMixin from 'react-addons-pure-render-mixin';
-import { Card, CardTitle } from 'react-toolbox/lib/card';
+import { Card } from 'react-toolbox/lib/card';
+import ScoreBoxList from './ScoreBoxList';
+import mui from 'material-ui';
 
 export default React.createClass({
   mixins: [PureRenderMixin],
   render() {
     return (
       <div className="ScoreBox">
-        <Card style={{ width: '300px', height: '300px' }}>
-          <CardTitle
-            title="Patient Experience"
-            avatar="https://s31.postimg.org/ihdkqz7ez/Information.png"
-          />
+        <Card style={{ width: '400px', height: '1000px' }}>
+          <ScoreBoxList>
+          </ScoreBoxList>
         </Card>
       </div>
     );
--- src/components/ScoreBox/ScoreBoxList.jsx
+++ src/components/ScoreBox/ScoreBoxList.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import Link from 'react-toolbox/lib/link';
 import PureRenderMixin from 'react-addons-pure-render-mixin';
+import { Card, CardTitle } from 'react-toolbox/lib/card';
+import mui from 'material-ui';
+import AlarmIcon from 'react-material-icons/icons/action/lock';
+import FontIcon from 'react-toolbox/lib/font_icon';
+import { List, ListItem, ListSubHeader, ListDivider, ListCheckbox } from 'react-toolbox/lib/list';
 
 export default React.createClass({
 mixins: [PureRenderMixin],
@@ -8,11 +13,46 @@ mixins: [PureRenderMixin],
     return (
   <div>
  <nav>
-    <Link active href="/#/components/link" label="Peer Comparison"/>
-    <Link href="/#/components/link" label="Change Over Time"/>
-        <Link href="/#/components/link" label="Show Detail"/>
+   <List selectable ripple>
+    <ListItem
+      caption='Patient Experience'
+      rightIcon='info'
+      value='info'
+    />
+   <h10><ListItem
+      caption='2/10'
+      leftIcon='info'
+      legend="(label/define?)"
+      value='info'
+    /></h10>
+     <h10><ListItem
+      caption='Peer Comparison'
+      leftIcon='open_in_browser'
+      rightIcon='info'
+      value='info'
+    /></h10>
+    <h10><ListItem
+      caption='Change Over Time'
+      leftIcon='add'
+      rightIcon='info'
+      value='info'
+    /></h10>
+      <ListItem
+      caption='Show Detail'
+    />
+    </List>
+    <p> Patient
+    Experience <FontIcon value='info' /> </p>
+    <h1> <FontIcon value='info' /> 3/15</h1>
+    <h2> (label/define?) </h2>
+    <p1> <FontIcon value='person'/>Peer Comparison</p1>
+    <p2> 5/10 </p2>
+    <p1> <FontIcon value='open_in_browser'/>Change Over Time</p1>
+    <p3> UP</p3>
+    <Link href="#/components/link" label="Show Detail"/>
   </nav>
   </div>
+
 );
 }
 });
