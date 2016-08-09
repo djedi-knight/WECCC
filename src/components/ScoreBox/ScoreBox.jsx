cf51bc64c874c70f975e74dd91ee534eb254b9ab
Shawn Daichendt <shawn.daichendt@gmail.com>
2016-08-09 13:58:36 -0400

    refactored/ linted ScoreBox code

--- src/components/ScoreBox/ScoreBox.jsx
+++ src/components/ScoreBox/ScoreBox.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
-import PureRenderMixin from 'react-addons-pure-render-mixin';
-import { Card } from 'react-toolbox/lib/card';
-import ScoreBoxList from './ScoreBoxList';
-import mui from 'material-ui';
+import { Card, FontIcon, Link } from 'react-toolbox';
 
 export default React.createClass({
-  mixins: [PureRenderMixin],
   render() {
     return (
       <div className="ScoreBox">
         <Card style={{ width: '400px', height: '1000px' }}>
-          <ScoreBoxList>
-          </ScoreBoxList>
+          <p> Patient Experience <FontIcon value="info" /> </p>
+          <h1> <FontIcon value="info" /> 3/15</h1>
+          <h2> (label/define?) </h2>
+          <p1> <FontIcon value="person" /> Peer Comparison </p1>
+          <p2> 5/10 </p2>
+          <p1> <FontIcon value="open_in_browser" />Change Over Time</p1>
+          <p3> UP</p3>
+          <Link href="#/" label="Show Detail" />
         </Card>
       </div>
     );
