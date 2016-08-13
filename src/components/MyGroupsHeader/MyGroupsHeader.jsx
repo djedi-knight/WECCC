2838db853dc7bcd4fc3eb190d362ffd077946738
Shawn Daichendt <shawn.daichendt@gmail.com>
2016-08-13 12:10:00 -0400

    refactored MyGroupsHeader

--- src/components/MyGroupsHeader/MyGroupsHeader.jsx
+++ src/components/MyGroupsHeader/MyGroupsHeader.jsx
@@ -1,12 +1,7 @@
 import React from 'react';
 import PureRenderMixin from 'react-addons-pure-render-mixin';
-import Dropdown from 'react-toolbox/lib/dropdown';
-import Navigation from 'react-toolbox/lib/navigation';
-import {Button,IconButton,FontIcon} from 'react-toolbox/lib/button';
-import Dialog from 'react-toolbox/lib/dialog';
+import { Dropdown, FontIcon } from 'react-toolbox';
 import theme from '../../theme/default';
-import MyGroupsPopoversection from '../MyGroupsPopoversection/MyGroupsPopoversection.jsx';
-
 
 const countries = [
   { value: 'EN-gb', label: 'Patient Experience Score Box' },
@@ -15,55 +10,33 @@ const countries = [
   { value: 'EN-en', label: 'Self-Reliance Score Box' },
 ];
 
-
 export default React.createClass({
-    mixins: [PureRenderMixin],
-   getInitialState() {
+  mixins: [PureRenderMixin],
+  getInitialState() {
     return { active: false };
-},
-
-  toggleActiveState() {
-    this.setState({ active: !this.state.active });
   },
-
   render() {
     return (
-
-      <div className="MyGroupsHeader" style={{  background:'#E7F6ED', height:'90px',margin:'80px 0 0 0'}}>
-
-          <div className={theme.SortingSelector} > 
-             <Dropdown className={theme.dropdown}
-              label={'Sort by timeframe:'}
-              auto
-              onChange={this.handleChange}
-              source={countries}
-              value={this.value}
-              template={this.customItem}
-            />
-          </div>
-
-          <div className="MyGroupsLogo">
-
-            <h2 style={{ width:'80%', padding:'2%', displya:'inline'}}> MY Groups 
-              <span style={{margin: '0'}}> 
-                  <IconButton icon='info' onMouseOver={this.toggleActiveState} />
-              </span>
-            </h2>
-
-            <Dialog active={this.state.active} 
-                    onEscKeyDown={this.toggleActiveState} 
-                    onOverlayClick={this.toggleActiveState}     
-            >
-              <MyGroupsPopoversection />
-
-            </Dialog>
-
-          </div>              
-
+      <div className="MyGroupsHeader" style={{ background: '#E7F6ED', height: '90px', margin: '80px 0 0 0' }}>
+        <div className="MyGroupsLogo">
+          <h2 style={{ width: '80%', padding: '2%', displya: 'inline' }}> My Groups
+            <span style={{ margin: '0' }}>
+              <FontIcon value="info" />
+            </span>
+          </h2>
+        </div>
+        <div className={theme.SortingSelector}>
+          <Dropdown
+            className="dropdown"
+            label={'Sort by timeframe:'}
+            onChange={this.handleChange}
+            source={countries}
+            value={this.value}
+            template={this.customItem}
+            auto
+          />
+        </div>
       </div>
     );
   }
 });
-
-
-
