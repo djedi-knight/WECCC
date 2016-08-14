620ff469b8ebf20b0bed057f33e1a17c9f5ef769
chang11n <chang11n@uwindsor.ca>
2016-08-13 20:13:08 -0400

    fixed code

--- src/components/MyGroupsHeader/MyGroupsHeader.jsx
+++ src/components/MyGroupsHeader/MyGroupsHeader.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 import PureRenderMixin from 'react-addons-pure-render-mixin';
-import { Dropdown, FontIcon } from 'react-toolbox';
+import Dropdown from 'react-toolbox/lib/dropdown';
+import {Button,IconButton,FontIcon} from 'react-toolbox/lib/button';
 import theme from '../../theme/default';
+import Tooltip from 'react-toolbox/lib/tooltip';
+
+
+let TooltipButton = Tooltip(IconButton);
+
 
 const countries = [
   { value: 'EN-gb', label: 'Patient Experience Score Box' },
@@ -17,26 +23,34 @@ export default React.createClass({
   },
   render() {
     return (
-      <div className="MyGroupsHeader" style={{ background: '#E7F6ED', height: '90px', margin: '80px 0 0 0' }}>
-        <div className="MyGroupsLogo">
-          <h2 style={{ width: '80%', padding: '2%', displya: 'inline' }}> My Groups
-            <span style={{ margin: '0' }}>
-              <FontIcon value="info" />
-            </span>
-          </h2>
-        </div>
-        <div className={theme.SortingSelector}>
-          <Dropdown
-            className="dropdown"
-            label={'Sort by timeframe:'}
-            onChange={this.handleChange}
-            source={countries}
-            value={this.value}
-            template={this.customItem}
-            auto
-          />
-        </div>
+      // DropdownList Selector
+      <div className="MyGroupsHeader" style={{  background:'#E7F6ED', height:'90px',margin:'80px 0 0 0'}}>
+
+          <div className={theme.SortingSelector} > 
+             <Dropdown className={theme.dropdown}
+              label={'Sort by timeframe:'}
+              auto
+              onChange={this.handleChange}
+              source={countries}
+              value={this.value}
+              template={this.customItem}
+            />
+          </div>
+
+
+          <div className="MyGroupsLogo">
+
+            <h2 style={{ width:'80%', padding:'2%', displya:'inline'}}> MY Groups 
+              <span style={{margin: '0'}}> 
+                      <TooltipButton icon='info'  tooltip='{{ <MyGroupsPopoversection />}}'  tooltipPosition='right' />
+              </span>
+            </h2>
+
+          </div>              
+
       </div>
     );
   }
 });
+
+
