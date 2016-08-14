84bd998289f6e39d5818793bf883a3aa24d52f68
Shawn Daichendt <shawn.daichendt@gmail.com>
2016-08-14 12:17:27 -0400

    Updated styling for ScoreBox

--- src/components/ScoreBox/ScoreBox.jsx
+++ src/components/ScoreBox/ScoreBox.jsx
@@ -1,20 +1,40 @@
 import React from 'react';
-import { Card, FontIcon, Link } from 'react-toolbox';
+import PureRenderMixin from 'react-addons-pure-render-mixin';
+import { Card, CardTitle, CardText, CardActions, FontIcon, Link } from 'react-toolbox';
 import theme from '../../theme/default';
 
 export default React.createClass({
+  mixins: [PureRenderMixin],
   render() {
     return (
       <div className={theme.ScoreBox}>
-        <Card style={{ width: '25%', height: '400px', border: '2px solid gray' }}>
-          <p> Patient Experience <FontIcon value="info" /> </p>
-          <h1> <FontIcon value="info" /> 3/15</h1>
-          <h1> (label/define?) </h1>
-          <h1> <FontIcon value="person" /> Peer Comparison </h1>
-          <h1> 5/10 </h1>
-          <h1> <FontIcon value="open_in_browser" />Change Over Time</h1>
-          <h1> UP</h1>
-          <Link href="#/" label="Show Detail" />
+        <Card theme={theme} raised>
+          <CardTitle
+            theme={theme}
+            avatar={<FontIcon value="info" />}
+            title="Patient Experience"
+          />
+          <div className={theme.score}>
+            <CardTitle
+              theme={theme}
+              avatar={<FontIcon value="warning" />}
+              title="3/15"
+              subtitle="(label/define?)"
+            />
+          </div>
+          <CardText>
+            <FontIcon value="person" />
+            Peer Comparison
+            5 / 10
+          </CardText>
+          <CardText>
+            <FontIcon value="open_in_browser" />
+            Change Over Time
+            <FontIcon value="arrow_upward" />
+          </CardText>
+          <CardActions>
+            <Link href="#" label="Show Detail" />
+          </CardActions>
         </Card>
       </div>
     );
