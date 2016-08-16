671b28388a78ee282ec5ac3ba5b42a99b59d9364
Shawn Daichendt <shawn.daichendt@gmail.com>
2016-08-16 14:31:32 -0400

    refactored/ linted ListBox

--- src/components/ListBox/ListBox.jsx
+++ src/components/ListBox/ListBox.jsx
@@ -1,41 +1,8 @@
 import React from 'react';
 import PureRenderMixin from 'react-addons-pure-render-mixin';
-import FontIcon from 'react-toolbox/lib/font_icon';
-import Table from 'react-toolbox/lib/table';
+import { FontIcon, Table, Tooltip, Link, Dialog, Button } from 'react-toolbox';
+import ListBoxModalView from '../ListBoxModalView/ListBoxModalView';
 import theme from '../../theme/default';
-import Tooltip from 'react-toolbox/lib/tooltip';
-import Link from 'react-toolbox/lib/link';
-import Dialog from 'react-toolbox/lib/dialog';
-import ListBoxModalView from '../ListBoxModalView/ListBoxModalView.jsx';
-import { Button } from 'react-toolbox';
-
-
-// export const Modal = React.createClass({ 
-//  propTypes: { modal: React.PropTypes.func.isRequired},
-//   getInitialState() {
-//     return { active: false};
-//   },   
-
-//   handleToggle () {
-//     this.setState({active: !this.state.active});
-//   },
-//   render(){
-//     const actions = [{ label: "OK", onClick: this.handleToggle }];
-//     return(
-//       <div>
-//         <Dialog 
-//           actions={this.actions}
-//           active={this.state.active}
-//           onEscKeyDown={this.handleToggle}
-//           onOverlayClick={this.handleToggle}
-//           >
-//           <ListBoxModalView/>
-//         </Dialog>
-//       </div>
-//     );
-//   }
-
-// });
 
 const TooltipLink = new Tooltip(Link);
 const title = 'RESPONDING TO NEED';
@@ -43,18 +10,13 @@ const subtitle = 'Top 3 Clinical Assessment Protocols [CAPS]';
 const ListModel = {
   title: { type: String }
 };
-
 const list = [
   { title: <div>{title} <TooltipLink style={{ float: 'right' }} icon="info_outline" tooltip="info popup" /><br />{subtitle}</div> },
   { title: '1. Falls' },
-  { title: <div>2. Social Isolation 
-              <a href="#" style={{ float: 'right' }}>Show details</a>              
-            </div> },
+  { title: <div>2. Social Isolation <a href="#" style={{ float: 'right' }}>Show details</a></div> },
   { title: <div>3. Depression <FontIcon style={{ color: '#FF0000' }} value="warning" /><a href="#" style={{ float: 'right' }}>Show details</a></div> },
   { title: <small><a href="#"><FontIcon value="person" /> Show peer comparison</a></small> }
 ];
-
-// <TooltipLink style ={{color:'#FF0000'}}  icon='warning' tooltip='Warning info' />
 
 export default React.createClass({
   mixins: [PureRenderMixin],
@@ -62,25 +24,29 @@ export default React.createClass({
   getInitialState() {
     return { source: list, active: false };
   },
-
-  handleToggle () {
-    this.setState({active: !this.state.active});
+  handleToggle() {
+    this.setState({ active: !this.state.active });
   },
-
   render() {
     return (
       <div style={{ width: '50%', margin: '5px' }} >
-        <Table className={theme.ListBox} selectable={false} heading={false} model={ListModel} source={this.state.source} />
+        <Table
+          className={theme.ListBox}
+          selectable={false}
+          heading={false}
+          model={ListModel}
+          source={this.state.source}
+        />
         <br />
-        <Button label='List Box Modal View' onClick={this.handleToggle} />
-                  <Dialog 
-                    actions={this.actions}
-                    active={this.state.active}
-                    onEscKeyDown={this.handleToggle}
-                    onOverlayClick={this.handleToggle}
-                    >
-                    <ListBoxModalView/>
-                  </Dialog>
+        <Button label="List Box Modal View" onClick={this.handleToggle} />
+        <Dialog
+          actions={this.actions}
+          active={this.state.active}
+          onEscKeyDown={this.handleToggle}
+          onOverlayClick={this.handleToggle}
+        >
+          <ListBoxModalView />
+        </Dialog>
       </div>
     );
   }
