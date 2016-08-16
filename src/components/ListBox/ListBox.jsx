e43aaef6cf0d2abae1247be6460729ddff73ec9a
Dee <adepojua@uwindsor.ca>
2016-08-16 13:10:11 -0400

    ListBoxModal View Button

--- src/components/ListBox/ListBox.jsx
+++ src/components/ListBox/ListBox.jsx
@@ -6,32 +6,36 @@ import theme from '../../theme/default';
 import Tooltip from 'react-toolbox/lib/tooltip';
 import Link from 'react-toolbox/lib/link';
 import Dialog from 'react-toolbox/lib/dialog';
+import ListBoxModalView from '../ListBoxModalView/ListBoxModalView.jsx';
+import { Button } from 'react-toolbox';
 
 
-export const Modal = React.createClass({  
-  getInitialState() {
-    return { active: false};
-  },   
+// export const Modal = React.createClass({ 
+//  propTypes: { modal: React.PropTypes.func.isRequired},
+//   getInitialState() {
+//     return { active: false};
+//   },   
 
-  handleToggle () {
-    this.setState({active: !this.state.active});
-  },
-  render(){
-    return(
-      <div>
-        <Dialog 
-          actions={this.actions}
-          active={this.state.active}
-          onEscKeyDown={this.handleToggle}
-          onOverlayClick={this.handleToggle}
-          >
-          <ListBoxModalView/>
-        </Dialog>
-      </div>
-    );
-  }
+//   handleToggle () {
+//     this.setState({active: !this.state.active});
+//   },
+//   render(){
+//     const actions = [{ label: "OK", onClick: this.handleToggle }];
+//     return(
+//       <div>
+//         <Dialog 
+//           actions={this.actions}
+//           active={this.state.active}
+//           onEscKeyDown={this.handleToggle}
+//           onOverlayClick={this.handleToggle}
+//           >
+//           <ListBoxModalView/>
+//         </Dialog>
+//       </div>
+//     );
+//   }
 
-});
+// });
 
 const TooltipLink = new Tooltip(Link);
 const title = 'RESPONDING TO NEED';
@@ -56,13 +60,27 @@ export default React.createClass({
   mixins: [PureRenderMixin],
 
   getInitialState() {
-    return { source: list };
+    return { source: list, active: false };
   },
+
+  handleToggle () {
+    this.setState({active: !this.state.active});
+  },
+
   render() {
     return (
-      <div style={{ width: '80%', margin: 'auto' }} >
+      <div style={{ width: '50%', margin: '5px' }} >
         <Table className={theme.ListBox} selectable={false} heading={false} model={ListModel} source={this.state.source} />
         <br />
+        <Button label='List Box Modal View' onClick={this.handleToggle} />
+                  <Dialog 
+                    actions={this.actions}
+                    active={this.state.active}
+                    onEscKeyDown={this.handleToggle}
+                    onOverlayClick={this.handleToggle}
+                    >
+                    <ListBoxModalView/>
+                  </Dialog>
       </div>
     );
   }
