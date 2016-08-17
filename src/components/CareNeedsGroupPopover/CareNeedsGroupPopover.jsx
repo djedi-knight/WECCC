ea013bf8a95f2983e17883ff5d610ba619cd738f
Dee <adepojua@uwindsor.ca>
2016-08-17 13:37:16 -0400

    Care needs group popover style and added react-tooltip

--- src/components/CareNeedsGroupPopover/CareNeedsGroupPopover.jsx
+++ src/components/CareNeedsGroupPopover/CareNeedsGroupPopover.jsx
@@ -1,59 +1,63 @@
 import React from 'react';
 import PureRenderMixin from 'react-addons-pure-render-mixin';
 import { Card, CardMedia, CardTitle, CardText, CardActions } from 'react-toolbox/lib/card';
-
+import {PieChart} from 'react-d3';
+//import { VictoryPie } from 'victory-pie';
+var pieData = [
+  {label: 'High Risk', value: 50.0},
+  {label: 'Medium Risk', value: 35.0},
+  {label: 'Low Risk', value: 20.0}
+];
+var legendItems = ['High Risk','Medium Risk','Low Risk'];
 
 export default React.createClass({
   render() {
     return (
-        <div>
-            <Card style={{width:'500px'}}>
+        <div style={{width:'30%'}}>            
             <h2 style={{background: '#110b31', color: '#FFF', margin:'0px'}}>LIGHT CARE NEEDS GROUP</h2>
             <div style={{padding:'0px 12px 12px 12px'}}>
-                <div style={{border: '1px solid grey', float:'left', width:'320px'}}>
+                <div style={{height: '165px', border: '1px solid grey', float:'left', width:'70%', borderStyle: 'hidden solid hidden solid'}}>
                     <h4>Mostly stable.</h4>
                     <p style={{fontSize:'20px'}}>
                         Some limitations requiring support mostly without complicating factors.
                     </p>
                 </div>
-                <div style={{border: '1px solid grey', float:'left', width: '150px'}}>
-                    <p>NUMBER OF PATIENTS</p>
-                    <p>600 <br/>of 1,500</p>
-                    
+                <div style={{border: '1px solid grey', float:'right', padding:'1px', width: '25%', borderStyle: 'hidden solid hidden hidden'}}>
+                    <p style={{fontSize:'20px'}}>NUMBER OF PATIENTS</p>                    
+                    <p style={{textAlign: 'right'}}>600 <br/><small>of 1,500</small></p>              
                 </div>
-                <div style={{border: '1px solid grey', float:'left', width: '100%'}}>
-                <p></p>
+
+                <div style={{border: '1px solid grey', float:'left', padding:'1px'}}>
+                   
+                    <div style={{float:'left', padding:'5px'}}>                        
+                        <h4>RISK LEVEL BREAKDOWN </h4>
+                        <p>
+                            Patients are further categorized by their level of risk according to the InterRAI Assessment Urgency Algorithm (AUA).
+                        </p>                                                
+                    </div> 
+                     <div style={{float:'right'}}>
+                        <PieChart
+                          legend={legendItems}
+                          data={pieData}
+                          width={200}
+                          height={200}
+                          radius={100}                          
+                          showInnerLabels= {false}
+                          showOuterLabels = {false}
+                        />
+
+                    </div>                   
                 </div>
             </div>
-            <a style={{margin: '20px'}} href='#'>Read more about AUA</a>            
-            </Card>
+           {/* <a style={{margin: '20px'}} href='#'>Read more about AUA</a> */  }         
+            
             <br/>            
         </div>       
     );
   }
 });
 
-
-
-
-
-
-            // <div style={{border: '1px solid grey', padding: '10px'}}>
-            //     <h6>Mostly stable.</h6>
-            //     <p>
-            //         Some limitations requiring support mostly without complicating factors.
-            //     </p>
-            //     <p>NUMBER OF PATIENTS</p>
-            //     <p style={{float:'right'}} >600 OF 1,500</p>
-            // </div>
-            // <div>
-            // <h6>
-            //     RISK LEVEL BREAKDOWN
-            // </h6>
-            // <p>
-            //     Patients are further categorized by their level of risk according to the InterRAI Assessment Urgency Algorithm(AUA).
-            // </p>
-            // <p>Box High Risk</p>
-            // <p>Box Medium Risk</p>
-            // <p>Box Low Risk</p>
-            // </div>
+// <p>Legend</p>
+//                         <p>Box High Risk</p>
+//                         <p>Box Medium Risk</p>
+//                         <p>Box Low Risk</p>
--- src/components/CareNeedsGroupTabBar/CareNeedsGroupTabBar.jsx
+++ src/components/CareNeedsGroupTabBar/CareNeedsGroupTabBar.jsx
@@ -2,20 +2,27 @@ import React from 'react';
 import PureRenderMixin from 'react-addons-pure-render-mixin';
 import { Tab, Tabs } from 'react-toolbox';
 import CareNeedsGroupPopover from '../CareNeedsGroupPopover/CareNeedsGroupPopover.jsx';
+import ReactTooltip from 'react-tooltip';
 
-
+var lab = <a data-tip data-for='global'> Light: 40% </a>;
 export default React.createClass({
   mixins: [PureRenderMixin],
   render() {
     return (
       <div className="MyGroupsTab">
+       
         <Tabs>
-          <Tab label="Light:40%"><small>Primary content</small></Tab>
+          <Tab label={lab}><small>Primary content</small></Tab>
           <Tab label="Moderate:30%"><small>Secondary content</small></Tab>
           <Tab label="Complex:20%" disabled><small>Disabled content</small></Tab>
           <Tab label="Fourth" hidden><small>Fourth content hidden</small></Tab>
           <Tab label="EOL:10%"><small>Fifth content</small></Tab>
         </Tabs>
+
+        <ReactTooltip id='global' aria-haspopup='true' type="light">
+         <CareNeedsGroupPopover/>
+        </ReactTooltip>
+
         <CareNeedsGroupPopover/>
       </div>
     );
