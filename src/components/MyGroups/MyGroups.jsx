1c3e34d7abe896f0d38c78c2fe4d0c75bfa507a6
Shawn Daichendt <shawn.daichendt@gmail.com>
2016-08-17 22:52:19 -0400

    refactored/ linted PopulationReach

--- src/components/MyGroups/MyGroups.jsx
+++ src/components/MyGroups/MyGroups.jsx
@@ -7,7 +7,6 @@ import CareNeedsGroupTabBar from '../CareNeedsGroupTabBar/CareNeedsGroupTabBar';
 import HealthOutcomesSection from '../HealthOutcomesSection/HealthOutcomesSection';
 import AreasOfFocusSection from '../AreasOfFocusSection/AreasOfFocusSection';
 import AlertSummarySection from '../AlertSummarySection/AlertSummarySection';
-import PopulationReachPage from '../PopulationReachPage/PopulationReachPage';
 
 export const MyGroups = React.createClass({
   mixins: [PureRenderMixin],
