5a769df70f45ef37fdfd850c879802d02f578c1b
Shawn Daichendt <shawn.daichendt@gmail.com>
2016-08-19 12:21:52 -0400

    added styles for AlertSummarySidebar

--- src/components/AlertSummarySidebar/AlertSummarySidebar.jsx
+++ src/components/AlertSummarySidebar/AlertSummarySidebar.jsx
@@ -1,24 +1,28 @@
 import React from 'react';
 import PureRenderMixin from 'react-addons-pure-render-mixin';
-import { Card, CardText } from 'react-toolbox/lib/card';
-import Button from 'react-toolbox/lib/button';
-
+import {
+  Button,
+  Card,
+  CardText
+} from 'react-toolbox';
+import style from './style';
 
 export default React.createClass({
   mixins: [PureRenderMixin],
   render() {
     return (
-      <div className="AlartSummarySidebar">
-        <Card style={{ width: '25%', height: '550px', border: '1px solid #EBE8E8', float: 'left' }}>
-          <span style={{ background: 'gray', margin: '0px', padding: '25px' }} />
+      <div className={style.alertSummarySidebar}>
+        <Card theme={style}>
+          <div className={style.headerBar} />
           <CardText>
             <div>
-              <p >This summary provides an overview of key areas of improvement.
+              <p>
+                This summary provides an overview of key areas of improvement.
                 Explore the information in these sections to discover other potential
                 opportunities for success.
               </p>
               <hr />
-              <Button style={{ padding: '2.5px', background: '#ABA7A7' }} label="DOWNLOAD OVERVIEW" href="#/download" />
+              <Button theme={style} label="DOWNLOAD OVERVIEW" href="#/download" />
             </div>
           </CardText>
         </Card>
