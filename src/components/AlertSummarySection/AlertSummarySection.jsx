03a24aec4538e2d0f20e373bc9fff2f54a0fc8a3
Shawn Daichendt <shawn.daichendt@gmail.com>
2016-08-19 12:23:33 -0400

    minor code update to AlertSummarySection

--- src/components/AlertSummarySection/AlertSummarySection.jsx
+++ src/components/AlertSummarySection/AlertSummarySection.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import PureRenderMixin from 'react-addons-pure-render-mixin';
 import AlertSummarySidebar from '../AlertSummarySidebar/AlertSummarySidebar';
 import AlertSummaryTable from '../AlertSummaryTable/AlertSummaryTable';
 
 export default React.createClass({
+  mixins: [PureRenderMixin],
   render() {
     return (
       <div>
