3257474d9df1cd76820cabcf8e736535a98e4d9e
stephenwaskew <stephenwaskew@gmail.com>
2016-08-22 14:43:10 -0400

    created a show more or less button

--- src/components/AreasOfFocusBody/index.jsx
+++ src/components/AreasOfFocusBody/index.jsx
@@ -2,13 +2,27 @@ import React from 'react';
 import PureRenderMixin from 'react-addons-pure-render-mixin';
 import ListBox from '../ListBox';
 import style from './style';
+import {Button} from 'react-toolbox';
+import ScoreBox from '../ScoreBox';
 
 export default React.createClass({
   mixins: [PureRenderMixin],
+
+    getInitialState() {
+    return { isShowMore: false};
+  },
+
+  handleToggle() {
+    this.setState({ isShowMore: !this.state.isShowMore });
+  },
+
   render() {
     return (
       <div className={style.AreasOfFocusBody} >
         <ListBox />
+
+          {this.state.isShowMore ?  <div> <ScoreBox /> <ScoreBox /> </div> : null}
+        <Button label="Show More/Less" onClick={this.handleToggle} />
       </div>
     );
   }
