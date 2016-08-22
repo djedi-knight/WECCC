bdbbdf760316ea929b658906d2eca69658788bfd
Shawn Daichendt <shawn.daichendt@gmail.com>
2016-08-22 16:13:20 -0400

    refactored/ linted AreaOfFocusBody

--- src/components/AreasOfFocusBody/index.jsx
+++ src/components/AreasOfFocusBody/index.jsx
@@ -1,27 +1,43 @@
 import React from 'react';
 import PureRenderMixin from 'react-addons-pure-render-mixin';
+import { Row, Col } from 'react-flexbox-grid';
+import { Button } from 'react-toolbox';
+import ScoreBox from '../ScoreBox';
 import ListBox from '../ListBox';
 import style from './style';
-import {Button} from 'react-toolbox';
-import ScoreBox from '../ScoreBox';
+
+export const ShowMoreSection = React.createClass({
+  mixins: [PureRenderMixin],
+  render() {
+    return (
+      <Row>
+        <Col xs={4}>
+          <ScoreBox />
+        </Col>
+        <Col xs={4}>
+          <ScoreBox />
+        </Col>
+        <Col xs={4}>
+          <ScoreBox />
+        </Col>
+      </Row>
+    );
+  }
+});
 
 export default React.createClass({
   mixins: [PureRenderMixin],
-
-    getInitialState() {
-    return { isShowMore: false};
+  getInitialState() {
+    return { isShowMore: false };
   },
-
   handleToggle() {
     this.setState({ isShowMore: !this.state.isShowMore });
   },
-
   render() {
     return (
       <div className={style.AreasOfFocusBody} >
         <ListBox />
-
-          {this.state.isShowMore ?  <div> <ScoreBox /> <ScoreBox /> </div> : null}
+        {this.state.isShowMore ? <ShowMoreSection /> : null}
         <Button label="Show More/Less" onClick={this.handleToggle} />
       </div>
     );
