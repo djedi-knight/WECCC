6469d6b0115bd339492bac7068b52c05a511a73e
Shawn Daichendt <shawn.daichendt@gmail.com>
2016-08-23 12:20:37 -0400

    refactored/ linted CommunityTapestry

--- src/components/CommunityTapestry/index.jsx
+++ src/components/CommunityTapestry/index.jsx
@@ -2,21 +2,18 @@ import React from 'react';
 import PureRenderMixin from 'react-addons-pure-render-mixin';
 import { connect } from 'react-redux';
 import * as actionCreators from '../../actions/action_creators';
-
-import AppBar from 'react-toolbox/lib/app_bar';
-import { Card, CardText} from 'react-toolbox';
+import { AppBar, Card, CardText, FontIcon } from 'react-toolbox';
 import { PieChart } from 'react-d3';
-import FontIcon from 'react-toolbox/lib/font_icon';
-import data from './pieData.json';
-
+import style from './style';
+import data from './data.json';
 
 export const CommunityTapestry = React.createClass({
   mixins: [PureRenderMixin],
   render() {
     return (
-      <div style={{border:'1px solid #CCC'}} >
-        <AppBar><h1>Your Community Pattern</h1></AppBar>
-        <div style={{textAlign: 'center', margin:'40px' }}>
+      <div className={style.communityTapestry}>
+        <AppBar><h1>Community Tapestry Page</h1></AppBar>
+        <div style={{ textAlign: 'center', margin: '40px' }}>
           <PieChart
             data={data}
             width={400}
@@ -24,30 +21,28 @@ export const CommunityTapestry = React.createClass({
             radius={200}
           />
         </div>
-        <div style={{margin:'20px', float:'centre'}} >
-
-        <div style={{margin:'20px', width:'20%', textAlign:'centre', display:'inline-block' }} >
-              <Card>
-                <CardText>
-                  <h3>S.R.O.I</h3>
-                  <h2>15</h2>
-                  <hr/>
-                  <FontIcon value='trending_down' />
-                </CardText>              
-              </Card>
-            </div>
-            <div style={{margin:'20px', width:'20%', textAlign:'centre', display:'inline-block' }} >
-              <Card>
-                <CardText>
-                  <h3>E.R.O.I</h3>
-                  <h2>27</h2>
-                  <hr/>
-                  <FontIcon value='trending_up' />
-                </CardText>              
-              </Card>
-            </div>
-            </div>
-
+        <div style={{ margin: '20px', float: 'centre' }}>
+          <div style={{ margin: '20px', width: '20%', textAlign: 'centre', display: 'inline-block' }}>
+            <Card>
+              <CardText>
+                <h3>S.R.O.I</h3>
+                <h2>15</h2>
+                <hr />
+                <FontIcon value="trending_down" />
+              </CardText>
+            </Card>
+          </div>
+          <div style={{ margin: '20px', width: '20%', textAlign: 'centre', display: 'inline-block' }}>
+            <Card>
+              <CardText>
+                <h3>E.R.O.I</h3>
+                <h2>27</h2>
+                <hr />
+                <FontIcon value="trending_up" />
+              </CardText>
+            </Card>
+          </div>
+        </div>
       </div>
     );
   }
