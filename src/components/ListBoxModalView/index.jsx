7fb1e9ae7bf1a26216287c9d66708dac97b43638
Dee <adepojua@uwindsor.ca>
2016-08-25 10:53:55 -0400

    Chart functionality with Victory chart

--- src/components/ListBoxModalView/index.jsx
+++ src/components/ListBoxModalView/index.jsx
@@ -3,6 +3,29 @@ import PureRenderMixin from 'react-addons-pure-render-mixin';
 import { Checkbox, FontIcon } from 'react-toolbox';
 import { Row, Col } from 'react-flexbox-grid';
 import style from './style';
+import { VictoryChart, VictoryAxis, VictoryTheme, VictoryLine } from 'victory/dist/victory';
+
+const lowRisk = [
+  {quarter: 1, earnings: 15000},
+  {quarter: 2, earnings: 12500},
+  {quarter: 3, earnings: 19500},
+  {quarter: 4, earnings: 13000}
+];
+
+const mediumRisk = [
+  {quarter: 1, earnings: 11500},
+  {quarter: 2, earnings: 13250},
+  {quarter: 3, earnings: 20000},
+  {quarter: 4, earnings: 15500}
+];
+
+const highRisk = [
+  {quarter: 1, earnings: 18000},
+  {quarter: 2, earnings: 13250},
+  {quarter: 3, earnings: 15000},
+  {quarter: 4, earnings: 12000}
+];
+
 
 const dummyText = 'Adverse drug effects are defined by Lorem Ipsum Sed ut perspiciatis, unde omnis iste natus error sit voluptatem accusantium doloremque laudantium';
 
@@ -30,11 +53,22 @@ export default React.createClass({
                 <FontIcon className={style.subtitleIcon} value="trending_up" />
               </div>
               {/* TODO: replace image tag with graphical component  */}
-              <img
-                alt="graph"
-                style={{ width: '200px', height: '150px', padding: '12px' }}
-                src="https://s31.postimg.org/hla3xitq3/imageedit.jpg"
-              />
+              <VictoryChart domainPadding={20} theme={VictoryTheme.material}>
+              <VictoryAxis   />
+              <VictoryAxis dependentAxis tickFormat={(x) => (`$${x / 1000}k`)}        />
+              <VictoryLine
+              data={lowRisk}
+              x={"quarter"}
+              y={"earnings"}    />
+              <VictoryLine
+                data={mediumRisk}
+                x={"quarter"}
+                y={"earnings"}  />
+              <VictoryLine
+                data={highRisk}
+                x={"quarter"}
+                y={"earnings"}  />
+            </VictoryChart> 
             </Row>
             <Row className={style.peerComparisonSection}>
               <div className={style.subtitle}>
