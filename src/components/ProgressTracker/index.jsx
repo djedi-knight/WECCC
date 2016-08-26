0dd5b929c215c297c632807551e353d1061e1186
chang11n <chang11n@uwindsor.ca>
2016-08-26 01:21:37 -0400

    update the table

--- src/components/ProgressTracker/index.jsx
+++ src/components/ProgressTracker/index.jsx
@@ -4,7 +4,8 @@ import { connect } from 'react-redux';
 import * as actionCreators from '../../actions/action_creators';
 import { FontIcon, Table } from 'react-toolbox';
 import style from './style';
-//import data from './data.json';
+import { Row, Col } from 'react-flexbox-grid';
+import data from './data.json';
 
 const ProgressTrackerTable = {
   INDICATOR: { type: String },
@@ -15,35 +16,64 @@ const ProgressTrackerTable = {
   COLOURCODE: { type: Number},
 };
 
-const data = [
+/*
+const templet = [
   { INDICATOR: <b>Income </b>, GOAL: '5%', BASELINE: 'A', CHANGE: '-2%', TREND: <FontIcon value="trending_down"/>, COLOURCODE: <div style={{color:'red'}}><FontIcon value="warning" /> </div> },
   { INDICATOR: <a href="#">Age</a>, GOAL: '21%', BASELINE: 'A',CHANGE: '1%',TREND: <FontIcon value="trending_up"/>, COLOURCODE: <div style={{color:'#CBCB1B'}}><FontIcon value="star" /> </div>  },
   { INDICATOR: <b>Education</b> , GOAL: '--', BASELINE: '--',TREND: <FontIcon value="trending_up"/>, COLOURCODE: <div style={{color:'green'}}><FontIcon value="check" /> </div> },
   { INDICATOR: <b>Air Pollution</b> , GOAL: '--', BASELINE: '--', COLOURCODE: <div style={{color:'blue'}}><FontIcon value="lens" /> </div> },
   { INDICATOR: <b>Climate</b> , GOAL: '--', BASELINE: '--'},
   { INDICATOR: <b>Life Expectancy</b> , GOAL: '--', BASELINE: '--'},
-
 ];
+*/
+
 
 export const ProgressTracker = React.createClass({
   mixins: [PureRenderMixin],
   getInitialState() {
     return { data };
   },
+
+
   render() {
     return (
 
-        <div className={style.progressTracker}>  
+      <div className={style.progressTracker}>  
           <div className={style.progressTrackerHeader}>
               <h2>Progress Tracker </h2>
           </div>
+
           <div className={style.progressTrackerTable}>
-            <Table theme={style} selectable={false} model={ProgressTrackerTable} source={this.state.data} />
+            <Table theme={style} selectable={false} model={ProgressTrackerTable} source={this.state.data.tableData} />
           </div>
-        </div>
+          <div> 
+              <Row className={style.tableHeader}>
+                      <Col xs={2} >INDICATOR</Col>
+                      <Col xs={2} >GOAL</Col>
+                      <Col xs={2}>BASELINE</Col>
+                      <Col xs={2}>CHANGE</Col>
+                      <Col xs={2}>TREND</Col>
+                      <Col xs={2}>COLOURCODE</Col>
+              </Row>
+          </div>
+                             
+                    {this.state.data.tableData.map((tableData, i) =>
+                      <div key={i} >        
+                      <Row className={style.tableRow}> 
+                        <Col xs={2}>    {tableData.INDICATOR}</Col>  
+                        <Col xs={2}>   {tableData.GOAL}</Col>  
+                        <Col xs={2}>   {tableData.BASELINE}</Col>  
+                        <Col xs={2}>   {tableData.CHANGE}</Col>  
+                        <Col xs={2}>   <FontIcon value={tableData.TREND}/>  </Col>                      
+                        <Col xs={2}>   <FontIcon style={{color: tableData.COLOURCODE}} value="lens" />  </Col>  
+                      </Row>   
+                      </div>
+                      )}         
+      </div> 
     );
   }
 });
+
 
 function mapStateToProps(state) {
   return {
