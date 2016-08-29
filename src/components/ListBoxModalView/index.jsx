33333e12b2bf3cf735178c4f0f4b7432ae334780
Dee <adepojua@uwindsor.ca>
2016-08-29 14:06:55 -0400

    Chart Function

--- src/components/ListBoxModalView/index.jsx
+++ src/components/ListBoxModalView/index.jsx
@@ -5,28 +5,27 @@ import { Row, Col } from 'react-flexbox-grid';
 import style from './style';
 import { VictoryChart, VictoryAxis, VictoryTheme, VictoryLine } from 'victory/dist/victory';
 import data from './highRisk.json';
-import {Button} from 'react-toolbox/lib/button';
 
-export const ChartSubgroups = React.createClass({
-  mixins: [PureRenderMixin],
-  render() {
-    return (
-      <div>
-      <VictoryLine
-        data={data}
-        x={"x"}
-        y={"y"}
-        style={{
-          data: {
-            stroke: "#F00",
-            strokeWidth: 3
-          }
-        }} 
-      />        
-      </div>
-    );
-  }
-});
+// export const ChartSubgroups = React.createClass({
+//   mixins: [PureRenderMixin],
+//   render() {
+//     return (
+//       <div>
+//       <VictoryLine
+//         data={data}
+//         x={"x"}
+//         y={"y"}
+//         style={{
+//           data: {
+//             stroke: "#F00",
+//             strokeWidth: 3
+//           }
+//         }} 
+//       />        
+//       </div>
+//     );
+//   }
+// });
 
 const lowRisk = [
   {x: 0, y: 150},
@@ -57,13 +56,9 @@ export default React.createClass({
   getInitialState() {
     return { check1: false, check2: false, check3: false};
   },
-  
-  // handleChart(){
-  //   this.setState({data: lowRisk});
-  // },
 
   handleChange(field, value) {
-    this.setState({ field, [field]: value});
+    this.setState({ [field]: value});
   },
 
   render() {
@@ -135,10 +130,9 @@ export default React.createClass({
                 <FontIcon className={style.subtitleIcon} value="person" />
               </div>
               <Checkbox checked={this.state.check1} name="HighRisk" label="- - High Risk" onChange={this.handleChange.bind(this, 'check1')} />
-              {this.state.value === 'HighRisk' ? <ChartSubgroups /> : null}
               <Checkbox checked={this.state.check2} label="- - Medium Risk" onChange={this.handleChange.bind(this, 'check2')} />
               <Checkbox checked={this.state.check3} label="- - Low Risk" onChange={this.handleChange.bind(this, 'check3')} />
-              */}
+             
               <div style={{ border: '1px solid gray', borderStyle: 'solid hidden hidden hidden' }}>
                 <p style={{ background: '#F2F7FA', color: '#110b31', margin: '0px', padding: '12px' }}>
                   {dummyText}
@@ -159,47 +153,3 @@ export default React.createClass({
     );
   }
 });
-
-
-// <VictoryLine
-//    padding={60}
-//    style={{
-//      data: {stroke: "red", strokeWidth: 9},
-//      labels: {fontSize: 15}
-//    }}
-//    interpolation={"linear"}
-//    data={[
-//      {x: 0, y: 1},
-//      {x: 1, y: 3},
-//      {x: 2, y: 2},      
-//      {x: 3, y: 4},
-//      {x: 4, y: 3},
-//      {x: 5, y: 5}
-//    ]}
-//    events={[{
-//     target: "data",
-//     eventHandlers: {
-//       onClick: () => {
-//         return [
-//           {
-//             mutation: (props) => {
-//               const { style } = props;
-//               return  style.stroke === "orange" ?
-//                 null :
-//                 {
-//                   style: merge(
-//                     {}, style, {stroke: "orange"}
-//                   )
-//                 };
-//             }
-//           }, {
-//             target: "labels",
-//             mutation: () => {
-//               return {text: "WOW"};
-//             }
-//           }
-//         ];
-//       }
-//     }
-//   }]}
-//  />
