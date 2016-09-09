87683eb494816479b1a68b999476c908bc0f21e4
Dee <adepojua@uwindsor.ca>
2016-09-09 11:54:25 -0400

    Implement Peer Comparison Checkbox functionality

--- src/components/ScoreBoxModal/index.jsx
+++ src/components/ScoreBoxModal/index.jsx
@@ -1,40 +1,18 @@
 import React from 'react';
 import PureRenderMixin from 'react-addons-pure-render-mixin';
-import { FontIcon } from 'react-toolbox';
+import { FontIcon, Checkbox } from 'react-toolbox';
 import { Row, Col } from 'react-flexbox-grid';
-import { VictoryChart, VictoryAxis, VictoryBar } from 'victory/dist/victory';
+import { VictoryChart, VictoryAxis, VictoryBar, VictoryStack, VictoryGroup } from 'victory/dist/victory';
 import style from './style';
+import data from './data.json';
 
 const dummyText = 'The DIVERT scale shows Lorem Ipsum Sed ut perspiciatis, unde omnis iste natus error sit voluptatem accusantium doloremque laudantium';
-
-const data = [
-  { x: 0, y: 4 }
-];
-
-export const Score = React.createClass({
-  mixins: [PureRenderMixin],
-  render() {
-    return (
-      <div>
-        <VictoryChart height={100}>
-          <VictoryAxis
-            tickValues={[0, 1, 2, 3, 4, 5]}
-            style={{
-              ticks: { stroke: 'red', size: 10 },
-              tickLabels: { fontSize: 22 },
-            }}
-          />
-          <VictoryBar horizontal data={data} />
-        </VictoryChart>
-      </div>
-    );
-  }
-});
-
+const b= 4;
+const d = 3;
 export default React.createClass({
   mixins: [PureRenderMixin],
   getInitialState() {
-    return { check1: false };
+    return { check: false, data };
   },
   handleChange(field, value) {
     this.setState({ [field]: value });
@@ -53,14 +31,20 @@ export default React.createClass({
               <div>
                 DIVERT
               </div>
-              <div style={{ width: '100%' }}>
-                <Score />
+              <div style={{ width: '100%' }}> 
+              <VictoryChart height={150} domain={{x:[0,5]}}> 
+                <VictoryAxis tickValues={[0, 1, 2, 3, 4, 5]} style={{ticks: { stroke: 'red', size: 10 }, tickLabels: { fontSize: 22 },}} />
+                <VictoryGroup horizontal height={100} offset={20} colorScale={"qualitative"} >
+                  <VictoryBar data={this.state.data.score} />
+                  {this.state.check ? <VictoryBar data={this.state.data.peer}/> : null }              
+                </VictoryGroup>
+              </VictoryChart>
               </div>
             </Row>
             <Row >
-              <div >
-                <FontIcon value="remove" /> My Outcomes <br />
-                <input type="checkbox" /> -- Peer Comparison
+              <div >              
+                <FontIcon value="remove" /> My Outcomes <br />        
+                <Checkbox checked={this.state.check} label="-- Peer Comparison" onChange={this.handleChange.bind(this, 'check')}/>
               </div>
               <div style={{ border: '1px solid gray', borderStyle: 'solid hidden hidden hidden' }}>
                 <p style={{ background: '#F2F7FA', color: '#110b31', margin: '0px', padding: '12px' }}>
@@ -69,7 +53,7 @@ export default React.createClass({
               </div>
             </Row>
           </Col>
-          <Col className={style.sidebar} xs={3}>
+          <Col className={style.sidebar} xs={3}>          
             <div style={{ border: '1px solid gray', width: '100%', float: 'right', height: '300px', background: '#FADEDE' }}>
               <div style={{ background: '#F00', margin: '0px' }}>
                 <FontIcon style={{ color: '#FFF', padding: '5px' }} value="warning" />
