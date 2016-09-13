6df760a6817d315bdaa5a7da1f2b824bc2b870a1
Dee <adepojua@uwindsor.ca>
2016-09-13 16:28:18 -0400

    GIS view page with map boundaries

--- src/components/GISPage/index.jsx
+++ src/components/GISPage/index.jsx
@@ -3,11 +3,10 @@ import PureRenderMixin from 'react-addons-pure-render-mixin';
 import { connect } from 'react-redux';
 import * as actionCreators from '../../actions/action_creators';
 import { RadioButton, RadioGroup } from 'react-toolbox';
-import { Map, Marker, Popup, TileLayer, Polygon } from 'react-leaflet';
+import { Map, Marker, Popup, TileLayer, Polygon, Circle } from 'react-leaflet';
 import { Row, Col } from 'react-flexbox-grid';
 import style from './style';
-
- const polygon = [{lng: 42.0468, lat: -82.60544}, {lng: 42.0465, lat: -82.60542}, {lng: 42.0568, lat: -82.60524}];
+import data from './data.json';
 
 export const GISSubgroups = React.createClass({
   mixins: [PureRenderMixin],
@@ -29,10 +28,11 @@ export const GISPage = React.createClass({
   mixins: [PureRenderMixin],
   getInitialState() {
     return {
-      lng: 42.0468,
-      lat: -82.60544,
+      lng: 42.3149,
+      lat: -83.0364,
       zoom: 13,
-      value: null
+      value: null,
+      data
     };
   },
   handleChange(value) {
@@ -40,7 +40,6 @@ export const GISPage = React.createClass({
   },
   render() {
     const position = [this.state.lng, this.state.lat];
-   
 
     return (
       <div className={style.gisPage}>
@@ -49,23 +48,43 @@ export const GISPage = React.createClass({
         </div >
         <br/>
         <Row>
-        <Col xs={3}>
+        <Col xs={2}>
         <RadioGroup name="gis" value={this.state.value} onChange={this.handleChange}>
           <RadioButton label="WECCC Partners" value="wecccpartners" />
           <RadioButton label="Social Networks" value="socialNetworks" />
           <RadioButton label="Equity" value="equity" />
         </RadioGroup>
         </Col>
-        <Col xs={9}>
+        <Col xs={8}>
         <Map center={position} zoom={this.state.zoom}>
           <TileLayer url={'http://{s}.tile.osm.org/{z}/{x}/{y}.png'}/>
-          <Marker position={position}>          
+         {/* <Marker position={position}>          
             <Popup>
               <span>A pretty CSS3 popup. </span>
             </Popup>
-          </Marker>
-          <Polygon position={polygon}/>
+          </Marker>*/}
+          
+          <Polygon positions={this.state.data.polygon} color ={"red"}>
+            <Popup>
+              <span>East Region. </span>
+            </Popup>
+            </Polygon>
+          <Polygon positions={this.state.data.polygonn} color ={"green"}>
+          <Popup>
+              <span>West Region. </span>
+            </Popup>
+            </Polygon>
+          <Polygon positions={this.state.data.pollygon} >
+          <Popup>
+              <span>North Region. </span>
+            </Popup>
+            </Polygon>
         </Map>
+        </Col>
+        <Col xs={2}>
+        <div style={{border: "1px solid", backgroundColor:"red", width: "20px", height: "20px"}}></div>East Region
+        <div style={{ border: "1px solid", backgroundColor:"blue", width: "20px", height: "20px"}}></div>North Region
+        <div style={{border: "1px solid", backgroundColor:"green", width: "20px", height: "20px"}}></div>West Region
         </Col>
         </Row>
         <Row>
