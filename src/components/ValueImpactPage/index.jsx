ca14b971768e4d0a14176e6e7087694c98375abd
Dee <adepojua@uwindsor.ca>
2016-09-14 14:27:31 -0400

    Initial framework for Value and impact page

--- src/components/ValueImpactPage/index.jsx
+++ src/components/ValueImpactPage/index.jsx
@@ -3,8 +3,14 @@ import PureRenderMixin from 'react-addons-pure-render-mixin';
 import { connect } from 'react-redux';
 import * as actionCreators from '../../actions/action_creators';
 import { Row, Col } from 'react-flexbox-grid';
-import { Dropdown } from 'react-toolbox';
+import { Dropdown, Tab, Tabs } from 'react-toolbox';
+import { VictoryPie, VictoryLabel } from 'victory/dist/victory';
+import data from './data.json';
 import style from './style';
+import ScoreBoxSimple from '../ScoreBoxSimple';
+
+
+const labelStyle = { labels: { fill: 'white', fontSize: 9, padding: 50 } };
 
 const careNeeds = [
   { value: 'light', label: 'Light' },
@@ -26,42 +32,62 @@ const subgroup = [
 export const ValueImpactPage = React.createClass({
   mixins: [PureRenderMixin],
   getInitialState(){
-    return { value: 'light', value2: 'london', value3: 'faith' };
+    return { value: 'light', value2: 'london', value3: 'faith', data, inverseIndex: 1 };
+  },  
+  handleChange(value){
+    this.setState({value: value});
   },
-  
-  handleChange(value, value2, value3){
-    this.setState({value: value, value2: value2, value3:value3});
+  handleChange2(value){
+    this.setState({value2: value});
+  },
+  handleChange3(value){
+    this.setState({value3: value});
+  },
+  handleInverseTabChange(index){
+    this.setState({inverseIndex: index});
   },
   render() {
     return (
       <div className={style.valueImpact}>
         <Row>
           <Col xs= {3} >
-          <div className={style.valueImpactHeader}>Population Subgroup</div>
-           <Dropdown
-            
-            onChange={this.handleChange}
-            source={careNeeds}
-            value={this.state.value}
-          />
-          <Dropdown
-            
-            onChange={this.handleChange}
-            source={city}
-            value={this.state.value2}
-          />
-          <Dropdown
-            
-            onChange={this.handleChange}
-            source={subgroup}
-            value={this.state.value3}
-          />
+            <div className={style.valueImpactHeader}>Population Subgroup</div>
+            <Dropdown              
+              onChange={this.handleChange}
+              source={careNeeds}
+              value={this.state.value}
+            />
+            <Dropdown              
+              onChange={this.handleChange2}
+              source={city}
+              value={this.state.value2}
+            />
+            <Dropdown              
+              onChange={this.handleChange3}
+              source={subgroup}
+              value={this.state.value3}
+            />
           </Col>
           <Col xs= {6}>
-          <div className={style.valueImpactHeader}>Your Community Pattern</div>
+            <div className={style.valueImpactHeader}>Your Community Pattern</div>
+            <VictoryPie
+              style={labelStyle}
+              data={this.state.data.PieData}
+              colorScale={this.state.data.colorScale}              
+            ><VictoryLabel/>
+            </VictoryPie>
+            <div>
+            <ScoreBoxSimple title ={"S.R.O.I"} score={"15"} trend={"down"} />
+            <ScoreBoxSimple title ={"E.R.O.I"} score={"27"} trend={"up"}/>
+            </div>
           </Col>
           <Col xs= {3}>
-          <div className={style.valueImpactHeader}>Things To Consider</div>
+            <div className={style.valueImpactHeader}>Things To Consider</div>
+            <Tabs index={this.state.inverseIndex} onChange={this.handleInverseTabChange} inverse>
+              <Tab label='First'><small>First Content</small></Tab>
+              <Tab label='Second'><small>Second Content</small></Tab>
+              <Tab label='Third'><small>Third Content</small></Tab>
+            </Tabs>
           </Col>
         </Row> 
       </div>
