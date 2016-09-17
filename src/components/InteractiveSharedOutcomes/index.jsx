64b45018b618bc905aa285e7847d7d86fb6f97d4
Shawn Daichendt <shawn.daichendt@gmail.com>
2016-09-17 13:12:05 -0400

    refactored/ linted InteractiveSharedOutcomes

--- src/components/InteractiveSharedOutcomes/index.jsx
+++ src/components/InteractiveSharedOutcomes/index.jsx
@@ -10,103 +10,114 @@ import style from './style';
 
 export const InteractiveSharedOutcomes = React.createClass({
   mixins: [PureRenderMixin],
-  getInitialState(){
-    return{ 
-      data, 
-      value: 'disaggregate', 
-      value2: 'levelOfDependency', 
-      value3: 'gender', 
-      value4: 'sexualOrientation', 
+  getInitialState() {
+    return {
+      value: 'disaggregate',
+      value2: 'levelOfDependency',
+      value3: 'gender',
+      value4: 'sexualOrientation',
       value5: 'isolation',
-      radio : 'allPopulation'
+      radio: 'allPopulation',
+      data
     };
   },
-  handleChange(value){
-    this.setState({value: value});
+  handleChange(value) {
+    this.setState({ value });
   },
-  handleChange2(value){
-    this.setState({value2: value});
+  handleChange2(value2) {
+    this.setState({ value2 });
   },
-  handleChange3(value){
-    this.setState({value3: value});
+  handleChange3(value3) {
+    this.setState({ value3 });
   },
-  handleChange4(value){
-    this.setState({value4: value});
+  handleChange4(value4) {
+    this.setState({ value4 });
   },
-  handleChange5(value){
-    this.setState({value5: value});
+  handleChange5(value5) {
+    this.setState({ value5 });
   },
-  handleRadioButton(value){
-    this.setState({radio: value});
+  handleRadioButton(radio) {
+    this.setState({ radio });
   },
   render() {
     return (
       <div className={style.InteractiveSharedOutcomes}>
         <Row>
-          <Col xs= {3} >
-            <Dropdown              
+          <Col xs={3}>
+            <Dropdown
               onChange={this.handleChange}
               source={this.state.data.disaggregate}
               value={this.state.value}
             />
-            <Dropdown              
+            <Dropdown
               onChange={this.handleChange2}
               source={this.state.data.dependency}
               value={this.state.value2}
             />
-            <Dropdown              
+            <Dropdown
               onChange={this.handleChange3}
               source={this.state.data.gender}
               value={this.state.value3}
             />
-            <Dropdown              
+            <Dropdown
               onChange={this.handleChange4}
               source={this.state.data.orientation}
               value={this.state.value4}
             />
-            <Dropdown              
+            <Dropdown
               onChange={this.handleChange5}
               source={this.state.data.isolation}
               value={this.state.value5}
             />
-            <Button label='Ok' raised primary/>
+            <Button label="Ok" raised primary />
           </Col>
-          <Col xs= {6}> 
-          <div>Equity Gap<br/><br/>
-          <RadioGroup name='equity' value={this.state.radio} onChange={this.handleRadioButton}>
-            <RadioButton label='All Population' value='allPopulation'/>
-            <RadioButton label='Lowest Quarter' value='lowestQuarter' />
-          </RadioGroup>
-          </div>         
+          <Col xs={6}>
+            <div className={style.InteractiveSharedOutcomesHeader}>Equity Gap</div>
+            <RadioGroup name="equity" value={this.state.radio} onChange={this.handleRadioButton}>
+              <RadioButton label="All Population" value="allPopulation" />
+              <RadioButton label="Lowest Quarter" value="lowestQuarter" />
+            </RadioGroup>
             <div className={style.InteractiveSharedOutcomesHeader}>Quality of Life</div>
             <Row>
-              <Col xs={6}><ScoreBoxSimple title={"Grade"} score={"A+"} trend={"up"}/></Col>
-              <Col xs={6}><ScoreBoxSimple title={"Equity Gap"} score={"-2.5"} trend={"down"}/></Col>
+              <Col xs={6}>
+                <ScoreBoxSimple title={"Grade"} score={"A+"} trend={"up"} />
+              </Col>
+              <Col xs={6}>
+                <ScoreBoxSimple title={"Equity Gap"} score={"-2.5"} trend={"down"} />
+              </Col>
             </Row>
-            <br/>          
+            <br />
             <div className={style.InteractiveSharedOutcomesHeader}>Burden</div>
             <Row>
-              <Col xs={6}><ScoreBoxSimple title={"Grade"} score={"D"} trend={"down"}/></Col>
-              <Col xs={6}><ScoreBoxSimple title={"Equity Gap"} score={"-4"} trend={"down"}/></Col>
+              <Col xs={6}>
+                <ScoreBoxSimple title={"Grade"} score={"D"} trend={"down"} />
+              </Col>
+              <Col xs={6}>
+                <ScoreBoxSimple title={"Equity Gap"} score={"-4"} trend={"down"} />
+              </Col>
             </Row>
-            <br/>
+            <br />
             <div className={style.InteractiveSharedOutcomesHeader}>Reported Health</div>
             <Row>
-              <Col xs={6}><ScoreBoxSimple title={"Grade"} score={"C"} trend={"down"}/></Col>
-              <Col xs={6}><ScoreBoxSimple title={"Equity Gap"} score={"-10"} trend={"down"}/></Col>
-            </Row>           
+              <Col xs={6}>
+                <ScoreBoxSimple title={"Grade"} score={"C"} trend={"down"} />
+              </Col>
+              <Col xs={6}>
+                <ScoreBoxSimple title={"Equity Gap"} score={"-10"} trend={"down"} />
+              </Col>
+            </Row>
           </Col>
-          <Col xs= {3}>  
-          <br/>        
+          <Col xs={3}>
+            <br />
             {this.state.data.items.map((item, i) =>
-            <div key={i}>
-              {item}
-              <br />
-              <hr />
-            </div>
+              <div key={i}>
+                {item}
+                <br />
+                <hr />
+              </div>
             )}
           </Col>
-        </Row>         
+        </Row>
       </div>
     );
   }
