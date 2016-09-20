1504815aee9a43f7d5e71828833128e97e5ea8b6
Shawn Daichendt <shawn.daichendt@gmail.com>
2016-09-20 14:03:21 -0400

    added API integration for CommunityTapestry

--- src/components/CommunityTapestry/index.jsx
+++ src/components/CommunityTapestry/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Fetch from 'react-fetch';
 import PureRenderMixin from 'react-addons-pure-render-mixin';
 import { connect } from 'react-redux';
 import * as actionCreators from '../../actions/action_creators';
@@ -8,50 +9,103 @@ import style from './style';
 import data from './data.json';
 import { VictoryPie } from 'victory/dist/victory';
 
-const colorScale = [
-  '#D85F49',
-  '#F66D3B',
-  '#D92E1D',
-  '#D73C4C',
-  '#FFAF59',
-  '#E28300',
-  '#F6A57F',
-  '#FF0000'
-];
-const labelStyle = { labels: { fill: 'white', fontSize: 9, padding: 50 } };
-
 export const CommunityTapestry = React.createClass({
+  propTypes: {
+    title: React.PropTypes.string,
+    pieCharts: React.PropTypes.array,
+    scoreCards: React.PropTypes.array
+  },
   mixins: [PureRenderMixin],
+  getInitialState() {
+    return { data };
+  },
+  getPieChartFor(key) {
+    if (this.props.pieCharts) {
+      const index = this.props.pieCharts.findIndex(pieChart => pieChart.key === key);
+      return this.props.pieCharts[index].data;
+    }
+    return [];
+  },
+  getSubGroupFor(key) {
+    if (this.props.scoreCards) {
+      const index = this.props.scoreCards.findIndex(subGroup => subGroup.key === key);
+
+      return this.props.scoreCards[index];
+    }
+    return {};
+  },
+  getScoreCardFor(subGroupKey, scoreCardKey) {
+    const subGroup = this.getSubGroupFor(subGroupKey);
+    if (subGroup.list) {
+      const index = subGroup.list.findIndex(scoreCard => scoreCard.key === scoreCardKey);
+
+      return subGroup.list[index];
+    }
+    return {};
+  },
   render() {
+    const labelStyle = { labels: { fill: 'white', fontSize: 9, padding: 50 } };
+    const colorScale = [
+      '#D85F49',
+      '#F66D3B',
+      '#D92E1D',
+      '#D73C4C',
+      '#FFAF59',
+      '#E28300',
+      '#F6A57F',
+      '#FF0000'
+    ];
+
     return (
       <div className={style.communityTapestry}>
         <div className={style.communityTapestryHeader}>
-          Community Tapestry
+          {this.props.title}
         </div>
         <div className={style.pieChartContainer}>
           <VictoryPie
             style={labelStyle}
-            data={data}
+            data={this.getPieChartFor('community-tapestry-pie-chart')}
             colorScale={colorScale}
           />
         </div>
         <div className={style.subgroup}>
           <Row className={style.header}>
             <div className={style.title}>
-              Return On Investment
+              {this.getSubGroupFor('return-on-investment').title}
             </div>
           </Row>
           <Row className={style.body}>
             <Col xs={3} />
             <Col xs={3}>
-              <ScoreBoxSimple />
+              <ScoreBoxSimple
+                title={this.getScoreCardFor('return-on-investment', 'social').title}
+                score={this.getScoreCardFor('return-on-investment', 'social').score}
+                trend={this.getScoreCardFor('return-on-investment', 'social').trend}
+              />
             </Col>
             <Col xs={3}>
-              <ScoreBoxSimple />
+              <ScoreBoxSimple
+                title={this.getScoreCardFor('return-on-investment', 'economic').title}
+                score={this.getScoreCardFor('return-on-investment', 'economic').score}
+                trend={this.getScoreCardFor('return-on-investment', 'economic').trend}
+              />
             </Col>
             <Col xs={3} />
           </Row>
         </div>
+      </div>
+    );
+  }
+});
+
+export const APIContainer = React.createClass({
+  mixins: [PureRenderMixin],
+  render() {
+    return (
+      <div>
+        <Fetch url="http://localhost:8090/api/pages/community-tapestry-test-page">
+          <CommunityTapestry />
+        </Fetch>
       </div>
     );
   }
@@ -67,4 +121,4 @@ function mapStateToProps(state) {
 export const CommunityTapestryContainer = connect(
   mapStateToProps,
   actionCreators
-)(CommunityTapestry);
+)(APIContainer);
