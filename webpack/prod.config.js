3965b0208ada6f1971147f8e389c7b1d4ae2f133
Shawn Daichendt <shawn.daichendt@gmail.com>
2016-10-27 16:02:29 -0400

    updated package.json prod build command

--- webpack/prod.config.js
+++ webpack/prod.config.js
@@ -8,7 +8,7 @@ module.exports = {
     './src/index.jsx'
   ],
   output: {
-    path: path.resolve(__dirname, './dist'),
+    path: path.resolve(__dirname, '../dist'),
     publicPath: '/',
     filename: 'bundle.js'
   },
