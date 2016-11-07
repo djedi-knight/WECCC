ea211c440ce7ed064f67a10c36b80fd9a7b33503
Dee <adepojua@uwindsor.ca>
2016-11-06 19:13:07 -0500

    Update on GIS Page

--- src/components/GISView/index.jsx
+++ src/components/GISView/index.jsx
@@ -8,9 +8,6 @@ import { Row, Col } from 'react-flexbox-grid';
 import style from './style';
 import data from './data.json';
 import PilotData from './PilotData.json';
-//import Select from 'react-select';
-import Control from './Control.jsx';
-
 
 export const GISView = React.createClass({
   mixins: [PureRenderMixin],
@@ -18,22 +15,65 @@ export const GISView = React.createClass({
     return {
       zoom: 12,
       data,
-      currentSelection: data.options[0].value     
+      PilotData,
+      currentSelection: data.options[0].value
     };
   },
   handleSelectionChange(newSelection) {
     this.setState({ currentSelection: newSelection });
   },
-  getColor(d) {
-    return d > 128 ? '#800026' :
-           d > 64  ? '#BD0026' :
-           d > 32  ? '#E31A1C' :
-           d > 16  ? '#FC4E2A' :
-           d > 8   ? '#FD8D3C' :
-           d > 4   ? '#FEB24C' :
-           d > 2   ? '#FED976' :
-                      '#FFEDA0';
+  getColor(d){
+    return (
+      d > 128 ? '#800026':
+      d > 64  ? '#BD0026' :
+      d > 32  ? '#E31A1C' :
+      d > 16  ? '#FC4E2A' :
+      d > 8   ? '#FD8D3C' :
+      d > 4   ? '#FEB24C' :
+      d > 2   ? '#FED976' :
+      '#FFEDA0'
+    );
   },
+
+  getMax(key) { 
+    console.log('state: ' + this.state.PilotData);
+    var max = this.state.PilotData.features[0].properties[key];
+    for (var i=0 ; i < this.state.PilotData.features.length ; i++){
+      console.log('current percentage: ' + this.state.PilotData.features[i].properties[key]);
+      max = Math.max(parseInt(this.state.PilotData.features[i].properties[key]), max)
+    }
+    console.log('max: ' + max);
+    return max;     
+  },
+
+  getMin(key){
+    var min = this.state.PilotData.features[0].properties[key];
+    for (var i=0; this.state.PilotData.features.length > i; i++){
+      console.log('current percentage: ' + this.state.PilotData.features[i].properties[key]);
+      min = Math.min(parseInt(this.state.PilotData.features[i].properties[key]), min)
+    } 
+    console.log('min: ' + min);
+    //console.log( 'range:' + Math.range(2, 6));  
+     
+  },
+
+  getrange(start, stop)
+  {
+    var array = [];
+    start = getMax();
+    stop = getMin();
+
+    var length = stop - start; 
+
+    for (var i = 0; i <= length; i++) { 
+        array[i] = start;
+        start++;
+    };
+    console.log('range: ' +array);
+
+    return array;
+  },
+
 
   getStyle(feature) {
     return {
@@ -45,66 +85,58 @@ export const GISView = React.createClass({
       fillOpacity: 0.7
     };
   },
-   
+  getLegend(){
+  },
   render() {
     return (
       <div className={style.gisPage}>
         <div className={style.header}>
           Leamington Pilot
         </div>
+        {this.getMin('Pop_2011')}
+       
         <br />
-
         <Row>
-        {/*<Control />*/}
-          <Col xs={2}>
-          {/*<Select
-            value={this.state.currentSelection}
-            options={this.state.data.test}
-            onChange={this.handleSelectionChange}
-            clearable= {false}/>*/}
-          
-             
-           <Dropdown
-              auto
+          <Col xs={2}> 
+            <Dropdown
               onChange={this.handleSelectionChange}
-              source={this.state.data.test}
+              source={this.state.data.options}
               value={this.state.currentSelection}
             />
-
           </Col>
           <Col xs={8}>
-          <div className={style.mapView}>
+            <div className={style.mapView}>
               <Map center={this.state.data.map} zoom={this.state.zoom} maxBounds={this.state.data.bounds}>
                 <TileLayer url={'http://{s}.tile.osm.org/{z}/{x}/{y}.png'} />  
-                <GeoJson data={PilotData} style={this.getStyle}/>                
+                <GeoJson data={PilotData} style={this.getStyle}/>           
               </Map>
             </div>     
           </Col>
           <Col xs={2}>
             <div>Legend</div>
-            {this.state.data.legend.map((legend, i) =>
+            {/*{this.state.data.legend.map((legend, i) =>
               <div key={i}>
-                <FontIcon style={{ color: legend.color }} value="lens" />
-                {legend.title}
-                <br />
+                <FontIcon style={{ color: legend.color }} value="lens" /> 
+
                 <br />
               </div>
-            )}
+            )}*/}
           </Col>          
         </Row>        
       </div>
     );
   }
 });
-
 function mapStateToProps(state) {
   return {
     test: 'Works!',
     state
   };
 }
-
 export const GISViewContainer = connect(
   mapStateToProps,
   actionCreators
 )(GISView);
+
+
+ // <button onClick={this.getMax}>JIll</button>
