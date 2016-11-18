bff298da28f3bec117296b2b242b2be463cb1a41
Dee <adepojua@uwindsor.ca>
2016-11-18 13:59:55 -0500

    Refactored AlertSummarySidebar

--- src/components/AlertSummarySidebar/index.jsx
+++ src/components/AlertSummarySidebar/index.jsx
@@ -2,16 +2,18 @@ import React from 'react';
 import PureRenderMixin from 'react-addons-pure-render-mixin';
 import { Button } from 'react-toolbox';
 import style from './style';
+import config from './config.json';
 
 export default React.createClass({
   mixins: [PureRenderMixin],
+  getInitialState() {
+    return { config };
+  },
   render() {
     return (
       <div className={style.alertSummarySidebar}>
         <p>
-          This summary provides an overview of key areas of improvement.
-          Explore the information in these sections to discover other potential
-          opportunities for success.
+          {this.state.config.text}
         </p>
         <hr />
         <Button theme={style} label="DOWNLOAD" href="#/download" />
