a4f3c207605f459c9dd423984b836c0d53781044
Dee <adepojua@uwindsor.ca>
2016-11-22 14:54:05 -0500

    Removed return on investment section

--- src/components/CommunityTapestry/index.jsx
+++ src/components/CommunityTapestry/index.jsx
@@ -11,7 +11,7 @@ export const CommunityTapestry = React.createClass({
   propTypes: {
     title: React.PropTypes.string,
     pieCharts: React.PropTypes.array,
-    scoreCards: React.PropTypes.array
+    //scoreCards: React.PropTypes.array
   },
   mixins: [PureRenderMixin],
   getInitialState() {
@@ -36,25 +36,6 @@ export const CommunityTapestry = React.createClass({
     }
 
     return [];
-  },
-  getSubGroupFor(key) {
-    if (this.props.scoreCards) {
-      const index = this.props.scoreCards.findIndex(subGroup => subGroup.key === key);
-
-      return this.props.scoreCards[index];
-    }
-
-    return {};
-  },
-  getScoreCardFor(subGroupKey, scoreCardKey) {
-    const subGroup = this.getSubGroupFor(subGroupKey);
-    if (subGroup.list) {
-      const index = subGroup.list.findIndex(scoreCard => scoreCard.key === scoreCardKey);
-
-      return subGroup.list[index];
-    }
-
-    return {};
   },
   handleChartDetailSelectionChange(newSelection) {
     this.setState({ currentChartDetailSelection: newSelection });
@@ -124,24 +105,6 @@ export const CommunityTapestry = React.createClass({
               </div>
             </Col>
             <Col xs={1} />
-          </Row>
-        </div>
-        <div className={style.subgroup}>
-          <Row className={style.header}>
-            <div className={style.title}>
-              {this.getSubGroupFor(this.state.config.keys.subGroup.key).title}
-            </div>
-          </Row>
-          <Row className={style.body}>
-            {this.state.config.keys.subGroup.scoreCards.map((scoreCard, x) =>
-              <Col key={x} xs={3}>
-                <ScoreCard
-                  title={this.getScoreCardFor(this.state.config.keys.subGroup.key, scoreCard).title}
-                  score={this.getScoreCardFor(this.state.config.keys.subGroup.key, scoreCard).score}
-                  trend={this.getScoreCardFor(this.state.config.keys.subGroup.key, scoreCard).trend}
-                />
-              </Col>
-            )}
           </Row>
         </div>
       </div>
