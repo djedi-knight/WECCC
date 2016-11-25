7df7009a6b11ee7c1a78279ee4a7a049531f7516
Dee <adepojua@uwindsor.ca>
2016-11-25 14:51:31 -0500

    Refactored show more section on my groups page

--- src/components/AreasOfFocusBody/index.jsx
+++ src/components/AreasOfFocusBody/index.jsx
@@ -6,28 +6,61 @@ import ScoreCard from '../ScoreCard';
 import ListBox from '../ListBox';
 import style from './style';
 import data from './data.json';
+import config from './config.json';
 
 export const ShowMoreSection = React.createClass({
+  propTypes: {
+    subGroup: React.PropTypes.string,
+    scoreCards: React.PropTypes.array
+  },
   mixins: [PureRenderMixin],
   getInitialState() {
-    return { data };
+    return { data, config };
+  },
+  getKeysFor(subGroupKey) {
+    if (this.state.config) {
+      const index = this.state.config.keys.subGroups.findIndex(subGroup => subGroup.key === subGroupKey);
+
+      return this.state.config.keys.subGroups[index];
+    }
+
+    return [];
+  },
+  getSubGroupFor(key) {
+    if (this.props.scoreCards) {
+      const index = this.props.scoreCards.findIndex(subGroup => subGroup.key === key);
+
+      return this.props.scoreCards[index];
+    }
+
+    return {};
+  },
+  getScoreCardFor(subGroupKey, scoreCardKey) {
+    const subGroup = this.getSubGroupFor(subGroupKey);
+    if (subGroup.list) {
+      const index = subGroup.list.findIndex(scoreCard => scoreCard.key === scoreCardKey);
+
+      return subGroup.list[index];
+    }
+
+    return {};
   },
   render() {
     return (
       <div>
         <Row>
-        {this.state.data.scoreCards.map((scoreCard, i) =>
+        {this.getKeysFor(this.props.subGroup).scoreCards.map((scoreCard, i) =>        
           <Col key={i} xs={4}>
             <ScoreCard
-              title={scoreCard.title}
-              score={scoreCard.score}
-              peerScore={scoreCard.peerScore}
-              warning={scoreCard.warning}
-              trend={scoreCard.trend}
-              showDetails={scoreCard.showDetails}
+              title={this.getScoreCardFor(this.props.subGroup, scoreCard).title}
+              score={this.getScoreCardFor(this.props.subGroup, scoreCard).score}
+              peerScore={this.getScoreCardFor(this.props.subGroup, scoreCard).peerScore}
+              warning= {this.getScoreCardFor(this.props.subGroup, scoreCard).warning}
+              trend={this.getScoreCardFor(this.props.subGroup, scoreCard).trend}
+              showDetails={this.getScoreCardFor(this.props.subGroup, scoreCard).showDetails}
             />
           </Col>
-          )}
+        )}
         </Row>
       </div>
     );
