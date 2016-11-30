2d5ab4762252889392fb38d0852d354e415c86cd
Dee <adepojua@uwindsor.ca>
2016-11-30 13:36:09 -0500

    refactored MyGroups component

--- src/components/MyGroups/index.jsx
+++ src/components/MyGroups/index.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import PureRenderMixin from 'react-addons-pure-render-mixin';
-import { connect } from 'react-redux';
-import * as actionCreators from '../../actions/action_creators';
+import Fetch from 'react-fetch';
+// import { connect } from 'react-redux';
+// import * as actionCreators from '../../actions/action_creators';
 import MyGroupsHeader from '../MyGroupsHeader';
 import CareNeedsGroupTabBar from '../CareNeedsGroupTabBar';
 import AreasOfFocusSection from '../AreasOfFocusSection';
 import AlertSummarySection from '../AlertSummarySection';
+import config from './config.json';
 
 export const MyGroups = React.createClass({
   mixins: [PureRenderMixin],
@@ -21,14 +23,40 @@ export const MyGroups = React.createClass({
   }
 });
 
-function mapStateToProps(state) {
-  return {
-    test: 'Works!',
-    state
-  };
-}
+export const MyGroupsContainer = React.createClass({
+  propTypes: {
+    route: React.PropTypes.object
+  },
+  mixins: [PureRenderMixin],
+  getInitialState() {
+    return { config };
+  },
+  getURL() {
+    if (this.props.route.demoRoute) {
+      return this.state.config.demoAPI;
+    }
 
-export const MyGroupsContainer = connect(
-  mapStateToProps,
-  actionCreators
-)(MyGroups);
+    return this.state.config.prodAPI;
+  },
+  render() {
+    return (
+      <div>
+        <Fetch url={this.getURL()}>
+          <MyGroups />
+        </Fetch>
+      </div>
+    );
+  }
+});
+
+// function mapStateToProps(state) {
+//   return {
+//     test: 'Works!',
+//     state
+//   };
+// }
+
+// export const MyGroupsContainer = connect(
+//   mapStateToProps,
+//   actionCreators
+// )(MyGroups);
