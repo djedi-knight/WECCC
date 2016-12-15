800e5b629518c0b05977c9999fecdfba0f882b55
Shawn Daichendt <shawn.daichendt@gmail.com>
2016-12-15 15:43:49 -0500

    refactored AreasOfFocusBody

--- src/components/AreasOfFocusBody/index.jsx
+++ src/components/AreasOfFocusBody/index.jsx
@@ -4,60 +4,27 @@ import { Row, Col } from 'react-flexbox-grid';
 import { Button } from 'react-toolbox';
 import ScoreCard from '../ScoreCard';
 import ListBox from '../ListBox';
-import style from './style';
-import data from './data.json';
 import config from './config.json';
+import style from './style';
 
 export const ShowMoreSection = React.createClass({
-  propTypes: {
-    subGroup: React.PropTypes.string,
-    scoreCards: React.PropTypes.array
-  },
   mixins: [PureRenderMixin],
   getInitialState() {
-    return { data, config };
-  },
-  getKeysFor(subGroupKey) {
-    if (this.state.config) {
-      const index = this.state.config.keys.subGroups.findIndex(subGroup => subGroup.key === subGroupKey);
-
-      return this.state.config.keys.subGroups[index];
-    }
-
-    return [];
-  },
-  getSubGroupFor(key) {
-    if (this.props.scoreCards) {
-      const index = this.props.scoreCards.findIndex(subGroup => subGroup.key === key);
-
-      return this.props.scoreCards[index];
-    }
-
-    return {};
-  },
-  getScoreCardFor(subGroupKey, scoreCardKey) {
-    const subGroup = this.getSubGroupFor(subGroupKey);
-    if (subGroup.list) {
-      const index = subGroup.list.findIndex(scoreCard => scoreCard.key === scoreCardKey);
-
-      return subGroup.list[index];
-    }
-
-    return {};
+    return { config };
   },
   render() {
     return (
       <div>
         <Row>
-        {this.getKeysFor(this.props.subGroup).scoreCards.map((scoreCard, i) =>        
+        {this.state.config.scoreCards.map((scoreCard, i) =>
           <Col key={i} xs={4}>
             <ScoreCard
-              title={this.getScoreCardFor(this.props.subGroup, scoreCard).title}
-              score={this.getScoreCardFor(this.props.subGroup, scoreCard).score}
-              peerScore={this.getScoreCardFor(this.props.subGroup, scoreCard).peerScore}
-              warning= {this.getScoreCardFor(this.props.subGroup, scoreCard).warning}
-              trend={this.getScoreCardFor(this.props.subGroup, scoreCard).trend}
-              showDetails={this.getScoreCardFor(this.props.subGroup, scoreCard).showDetails}
+              title={scoreCard.title}
+              score={scoreCard.score}
+              peerScore={scoreCard.peerScore}
+              warning={scoreCard.warning}
+              trend={scoreCard.trend}
+              showDetails={scoreCard.showDetails}
             />
           </Col>
         )}
@@ -82,7 +49,7 @@ export default React.createClass({
     return (
       <div className={style.AreasOfFocusBody} >
         <ListBox />
-        {this.state.isShowMore ? <ShowMoreSection subGroup="windsor-subgroup" scoreCards={this.props.scoreCards}/> : null}
+        {this.state.isShowMore ? <ShowMoreSection /> : null}
         <Button label="Show More/Less" onClick={this.handleToggle} />
       </div>
     );
